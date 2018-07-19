183e82c71ef2df72c083a70979310d427fe6046e
Shan Zhao <39207218+ShanzzZ@users.noreply.github.com>
2018-07-18 20:29:52 -0400

    Update 2.Operators.java

--- 2.Operators.java
+++ 2.Operators.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
-import java.math.*;
 
+/*
+ *
+ Given the meal price, tip percent, tax percent
+ Find and print the meal's total cost
+ */
+	
 public class Solution{
 	public static void main(String[]args){
 	Scanner scan = new Scanner(System.in);
@@ -11,9 +16,10 @@ public class Solution{
 	scan.close();
 	
 	double tip = mealCost*tipPercent/100;
-    double tax = mealCost*taxPercent/100;
+        double tax = mealCost*taxPercent/100;
         
-    int total_cost = (int) Math.round(mealCost + tip + tax);
+        int total_cost = (int) Math.round(mealCost + tip + tax); 
+		//Math.ceil向上取整，Math.floor向下取整，Math.round四舍五入，注意语句前标记想要的数据类型int
 	
 	System.out.println("The total meal cost is "+totalCost+" dollars.");
 	}
