9a961ce988a859f6168404d49f0b5c802aeca74a
Shan Zhao <39207218+ShanzzZ@users.noreply.github.com>
2018-07-22 17:27:44 -0400

    Update 10.Binary Numbers.java

--- 10.Binary Numbers.java
+++ 10.Binary Numbers.java
@@ -12,17 +12,17 @@ public class Solution{
 		
 		scan.close();
 		
-		int max=0;
-		int count=0;
+		int max=0; //最大连续个数
+		int count=0; //累计专用
 		
 		while(n>0){
 		 if(n%2!=0){
-		 	 count++;
-		 	 if(count>max){
-		 	 	 max=count;
+		 	 count++; //若不可被整除，累计一次
+		 	 if(count>max){ 
+		 	 	 max=count; //将最大连续存在max中
 		 	 }
 		 }else{
-		 	 	 count = 0;
+		 	 	 count = 0; //若可被整除，则之前累计的次数归零（最近一次累计已存储在max中，不会丢失）
 		 	 }
 		 	 n = n/2;
 		 }
