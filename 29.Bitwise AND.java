5ba67e42b7511aa4e55745e51f8818c121915341
Shan Zhao <39207218+ShanzzZ@users.noreply.github.com>
2018-07-27 15:43:32 -0400

    Add files via upload

---
+++ 29.Bitwise AND.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/*
+ *Given set S. Find two integers, A and B (where A<B), from set S 
+ *The value of A&B is the maximum possible and also less than a given integer K
+ */
+ 
+class Solution{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int T = scan.nextInt();
+		
+		for(int i=0; i<T; i++){
+			int A = scan.nextInt();
+			int K = scan.nextInt();
+		
+		int max = 0;
+		
+		for(int j=1; j<A; j++){
+			for(int l=j+1; l<=A; l++){//A可能为0，故不使用A-1
+				int temp = j&l;
+			if(temp>max && temp<K)
+				max = temp;
+			}
+		}
+		
+		System.out.println(max);
+		}//注意括号位置
+	}
+}
+				
+			
+		
+		
+			
+		
+	
